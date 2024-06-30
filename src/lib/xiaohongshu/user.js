81893c1f2d4ee63e1b332cbb962f5c861d133007
JerryYang-30 <102589310+JerryYang-30@users.noreply.github.com>
2024-06-30 22:33:09 +0800

    Update user.js

--- src/lib/xiaohongshu/user.js
+++ src/lib/xiaohongshu/user.js
@@ -51,7 +51,8 @@ let deal = async (ctx) => {
 				title: noteCard.displayTitle,
 				// 现在必须要登陆才能获取笔记链接，所以文章link都是{url}了（noteId为空）。
 				// 而Qi Reader把文章链接当作文章Id（如果源里没有Id字段的话），所以所有文章共用一个链接就导致了新文章被判定重复。
-				link: `${url}/${noteCard.noteId}`,
+				// link: `${url}/${noteCard.noteId}`,
+				link: `${noteCard.cover.infoList.pop().url}`,
 				description: `<img src ="${noteCard.cover.infoList.pop().url}"><br>${noteCard.displayTitle}`,
 				author: noteCard.user.nickname,
 				upvotes: noteCard.interactInfo.likedCount,
