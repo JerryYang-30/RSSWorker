eb637968fa5c61d19695723fb037d28189e02830
JerryYang-30 <102589310+JerryYang-30@users.noreply.github.com>
2024-06-30 22:51:57 +0800

    Update user.js
    使用笔记封面作为guid

--- src/lib/xiaohongshu/user.js
+++ src/lib/xiaohongshu/user.js
@@ -46,8 +46,6 @@ let deal = async (ctx) => {
 	const renderNote = (notes) =>
 		notes.flatMap((n) =>
 			n.map(({ noteCard }) => ({
-				// Qi Reader用Id判断文章是否重复
-				guid: `${noteCard.cover.infoList.pop().url}`, // 使用封面链接作为ID,
 				title: noteCard.displayTitle,
 				// 现在必须要登陆才能获取笔记链接，所以文章link都是{url}了（noteId为空）。
 				// 而Qi Reader把文章链接当作文章Id（如果源里没有Id字段的话），所以所有文章共用一个链接就导致了新文章被判定重复。
@@ -55,6 +53,8 @@ let deal = async (ctx) => {
 				description: `<img src ="${noteCard.cover.infoList.pop().url}"><br>${noteCard.displayTitle}`,
 				author: noteCard.user.nickname,
 				upvotes: noteCard.interactInfo.likedCount,
+				// Qi Reader用Id判断文章是否重复
+				guid: `${noteCard.cover.infoList.pop().url}`, // 使用封面链接作为ID,
 			}))
 		);
 	const renderCollect = (collect) => {
